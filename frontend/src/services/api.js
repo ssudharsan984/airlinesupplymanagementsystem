488dc8ea69a6c00386c2ffaaddd0867ec44a7204
ssudharsan984 <ssudharsan984@gmail.com>
2026-04-13 20:55:13 +0530

    update project

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api'
+  baseURL: process.env.REACT_APP_API_URL || 'https://airlinesupplymanagementsystem-3.onrender.com/api'
 });
 
 export const getDashboard = () => API.get('/dashboard');
